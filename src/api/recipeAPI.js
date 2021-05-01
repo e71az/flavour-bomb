06a653d7bc9cf80f13ffc4d21d7eb65e09e51a07
Elias Castañeda <e71az@icloud.com>
2021-05-01 04:02:06 -0600

    Created async logic to fetch data from recipe API

---
+++ src/api/recipeAPI.js
@@ -0,0 +1,44 @@
+// A mock function to mimic making an async request for data
+// var requestOptions = {
+//   method: 'GET',
+//   redirect: 'follow',
+// };
+
+// fetch('www.themealdb.com/api/json/v1/1/filter.php?c=Seafood', requestOptions)
+//   .then((response) => response.text())
+//   .then((result) => console.log(result))
+//   .catch((error) => console.log('error', error));
+
+// export function fetchRecipe(amount = 'No data') {
+//   return new Promise((resolve) =>
+//     setTimeout(() => resolve({ data: amount }), 500)
+//   );
+// }
+
+// import fetch from 'node-fetch';
+// import 'regenerator-runtime/runtime';
+
+export const getAllRecipes = async (recipeContent = 'No data') => {
+  let API = 'www.themealdb.com/api/json/v1/1/filter.php?c=Seafood';
+
+  let response = '';
+  // let recipe;
+  try {
+    response = await fetch(API);
+
+    if (response.ok === false) {
+      throw Error('Something went wrong with the Recipe API');
+    }
+
+    recipeContent = await response.json();
+  } catch (error) {
+    API = 'www.themealdb.com/api/json/v1/1/filter.php?c=Seafood';
+    response = await fetch(API);
+    recipeContent = await response.json();
+  }
+
+  // return recipe.result;
+    return new Promise((resolve) =>
+      setTimeout(() => resolve({ data: recipeContent }), 1000)
+    );
+};
