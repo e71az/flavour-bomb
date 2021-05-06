42d8be73f7b648a50f7b326668c3c8a6704e3163
Elias Castañeda <e71az@icloud.com>
2021-05-06 02:24:19 -0600

    Removed Unusued comments

--- src/reducers/recipeSlice.js
+++ src/reducers/recipeSlice.js
@@ -87,20 +87,8 @@ export const {
   mealFilter,
 } = counterSlice.actions;
 
-// The function below is called a selector and allows us to select a value from
-// the state. Selectors can also be defined inline where they're used instead of
-// in the slice file. For example: `useSelector((state: RootState) => state.counter.value)`
 export const selectAllRecipes = (state) => state.recipe.value;
 export const selectRecipeById = (state, recipeId) =>
   state.recipe.value.find((recipe) => recipe.id === recipeId);
-
-// We can also write thunks by hand, which may contain both sync and async logic.
-// Here's an example of conditionally dispatching actions based on current state.
-// export const incrementIfOdd = (amount) => (dispatch, getState) => {
-//   const currentValue = selectCount(getState());
-//   if (currentValue % 2 === 1) {
-//     dispatch(incrementByAmount(amount));
-//   }
-// };
 
 export default counterSlice.reducer;
